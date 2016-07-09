3c54bd713b0fe0b798ca3e14e17ec08dbbd503e0
Zach de Koning <Zachoz@users.noreply.github.com>
2016-07-09 23:37:43 +1000

    Fix messages having spaces renoved
    I don't know how I stuffed this up to badly

--- telstra_sms.php
+++ telstra_sms.php
@@ -12,7 +12,7 @@ class TelstraSMS
         $this->recipient = $recipient;
         $this->message = $message;
 
-        $this->message = str_replace(" ", "", $message); // Replace space characters
+        $this->recipient = str_replace(" ", "", $recipient); // Remove space characters in phone number if present
     }
 
     public function send()
